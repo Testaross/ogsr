fe69b54c9f3a479d6a04471a5a8430de18fca08c
Testarossa <austin.w.burrows@gmail.com>
2022-12-08 04:21:31 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,11 +1,5 @@
-Config                          = {}
-Config.gsrUpdate                = 1 * 1000          
-Config.waterClean               = true              
-Config.waterCleanTime           = 30 * 1000         
-Config.gsrTime                  = 30 * 60          
-Config.gsrAutoRemove            = 10 * 60 * 1000    
-Config.gsrUpdateStatus          = 5 * 60 * 1000     
+Config                          = {}     
+Config.clearGSR = 15 
+Config.clearGSRinWater          = 1    
 
-Config.oxInventory = (GetResourceState('ox_inventory') ~= 'missing')
-Config.oxLib = (GetResourceState('ox_lib') ~= 'missing')
-Config.oxTarget = (GetResourceState('ox_target') ~= 'missing')
+
