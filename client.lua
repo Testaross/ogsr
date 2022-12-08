46b14522dbad771c7081d95fa7f9cd06eb5a49b9
Testarossa <austin.w.burrows@gmail.com>
2022-12-08 01:36:44 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -4,41 +4,18 @@ local shot = false
 local lastShot = 0
 local timeInWater = 0
 local beenInWater = false
+local GetGameTimer = GetGameTimer
+local inWater = false
 
 if LocalPlayer.state.shot then
     shot = true 
+    lastShot = LocalPlayer.state.lastShot
 end
 
-local function boomerremover()
-	while true do
-		local sleep = 1
-        if KillThread == true then
-            break
-        end
-        if IsPedShooting(ped) then
-                print('here')
-                lastShot = GetGameTimer()
-                shot = true
-                LocalPlayer.state:set('shot', true, true)
-                sleep = 1000
-        end
-        Wait(sleep)
-	end
-end
-
-
-lib.onCache('weapon', function(value)
-    if value then
-        KillThread = false
-        CreateThread(boomerremover)
-    else
-        KillThread = true
-    end
-end)
-
-CreateThread(function(boomerremover)
+CreateThread(function()
 	while true do
         Wait(100)
+        local shot = LocalPlayer.state.shot
         if shot == true then
             if IsEntityInWater(ped) then
                 if inWater == false then
@@ -53,6 +30,7 @@ CreateThread(function(boomerremover)
             end
         end
         local timer = GetGameTimer()
+        local lastShot = LocalPlayer.state.lastShot
         if shot and beenInWater and timer - timeInWater >= (Config.clearGSRinWater * 60 * 1000) then
             ClearPedBloodDamage(ped)
             ClearPedEnvDirt(ped)
@@ -66,7 +44,7 @@ CreateThread(function(boomerremover)
         end
         if shot and timer - lastShot >= (Config.clearGSR * 60 * 1000) then
             lib.notify({
-                description = 'GSR Washed off',
+                description = 'GSR has faded',
                 type = 'error'
             })
             LocalPlayer.state:set('shot', false, true)
@@ -80,9 +58,7 @@ exports.ox_target:addGlobalPlayer({
         icon = '',
         label = 'GSR Test',
         groups = 'police',
-        canInteract = function(entity, distance, coords, name)
-            return distance < 1.5 
-        end,
+	distance = 1.5,
         onSelect = function(data)
             TriggerServerEvent('gsrTest', GetPlayerServerId(NetworkGetPlayerIndexFromPed(data.entity)))
         end
