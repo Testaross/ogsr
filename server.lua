5ba038a1147fc3b8eaea559e1c79656a3cc14898
Testarossa <austin.w.burrows@gmail.com>
2022-12-08 01:37:06 -0500

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,13 +1,18 @@
-
 RegisterNetEvent('gsrTest', function(target)
 	local src = source
 	local ply = Player(target)
 	if ply.state.shot == true then
-		print('yes')
         TriggerClientEvent('ox_lib:notify', src, {type = 'success', description = 'Test comes back POSITIVE (Has Shot)'})
 	else
         TriggerClientEvent('ox_lib:notify', src, {type = 'error', description = 'Test comes back NEGATIVE (Has Not Shot)'})
 	end
+end)
+
+RegisterNetEvent('ox_inventory:updateWeapon', function(action)
+    if action ~= 'ammo' then return end
+    local lastShot = GetGameTimer()
+    Player(source).state:set('shot', true, true)
+    Player(source).state:set('lastShot', lastShot, true)
 end)
 
 
