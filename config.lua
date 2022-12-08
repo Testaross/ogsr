7c6ced957722c8b31d29159a75da1f908009b8fb
Pickle <111543470+PickleModifications@users.noreply.github.com>
2022-12-08 04:03:52 -0500

    Added Standalone Support

--- config.lua
+++ config.lua
@@ -6,3 +6,37 @@ Config.gsrTime                  = 30 * 60
 Config.gsrAutoRemove            = 10 * 60 * 1000    
 Config.gsrUpdateStatus          = 5 * 60 * 1000     
 
+Config.oxInventory = (GetResourceState('ox_inventory') ~= 'missing')
+Config.oxLib = (GetResourceState('ox_lib') ~= 'missing')
+
+function AddTarget()
+    if Config.oxInventory then 
+        exports.ox_target:addGlobalPlayer({
+            {
+                icon = '',
+                label = 'GSR Test',
+                groups = 'police',
+                distance = 1.5,
+                onSelect = function(data)
+                    TriggerServerEvent('gsrTest', GetPlayerServerId(NetworkGetPlayerIndexFromPed(data.entity)))
+                end
+            }
+        })
+    end
+end
+
+function ShowNotification(data)
+    if Config.oxLib then 
+        lib.notify(data)
+    else
+        BeginTextCommandThefeedPost('STRING')
+        AddTextComponentSubstringPlayerName(data.description)
+        EndTextCommandThefeedPostTicker(false, true)
+    end
+end
+
+function PermissionCheck(name)
+    if (name == "gsr") then 
+        return true
+    end
+end
